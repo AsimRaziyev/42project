91f2a714344902513faf4cd403f0eff451283686
Assym <asimo_555@mail.ru>
2022-06-25 03:24:51 +0600

    changes_init

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-# from webapp.views import index_view, create_article
 from webapp.views import sum_calc
 
 urlpatterns = [
-    # path("", index_view),
-    # path("articles/add/", create_article)
-      path("", sum_calc),
+    path("", sum_calc),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,29 +1,5 @@
 from django.shortcuts import render
 
-#
-# def index_view(request):
-#     query = request.GET.getlist("name", "ttttt")
-#     print(query)
-#     context = {"name": query, "test": "lalala"}
-#     return render(request, "index.html", context)
-#
-#
-# def create_article(request):
-#     if request.method == "GET":
-#         return render(request, "create.html")
-#     else:
-#          user = {
-#              "name": "Test",
-#              "age": 25,
-#              "email": "test@mail.ru"
-#          }
-#          context = {
-#              "title": request.POST.get("title"),
-#              "author": request.POST.get("author"),
-#              "content": request.POST.get("content"),
-#              # "user": user
-#          }
-#          return render(request, 'article_view.html', context)
 
 def sum_calc(request):
     if request.method == "GET":
