beb031087d38c824b859b25cb7e46c6fe05c174f
Assym <asimo_555@mail.ru>
2022-06-25 03:22:14 +0600

    changes_init

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index_view, create_article
+# from webapp.views import index_view, create_article
+from webapp.views import sum_calc
 
 urlpatterns = [
-    path("", index_view),
-    path("articles/add/", create_article)
+    # path("", index_view),
+    # path("articles/add/", create_article)
+      path("", sum_calc),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,26 +1,73 @@
 from django.shortcuts import render
 
+#
+# def index_view(request):
+#     query = request.GET.getlist("name", "ttttt")
+#     print(query)
+#     context = {"name": query, "test": "lalala"}
+#     return render(request, "index.html", context)
+#
+#
+# def create_article(request):
+#     if request.method == "GET":
+#         return render(request, "create.html")
+#     else:
+#          user = {
+#              "name": "Test",
+#              "age": 25,
+#              "email": "test@mail.ru"
+#          }
+#          context = {
+#              "title": request.POST.get("title"),
+#              "author": request.POST.get("author"),
+#              "content": request.POST.get("content"),
+#              # "user": user
+#          }
+#          return render(request, 'article_view.html', context)
 
-def index_view(request):
-    query = request.GET.getlist("name", "ttttt")
-    print(query)
-    context = {"name": query, "test": "lalala"}
-    return render(request, "index.html", context)
-
-
-def create_article(request):
+def sum_calc(request):
     if request.method == "GET":
-        return render(request, "create.html")
-    else:
-         user = {
-             "name": "Test",
-             "age": 25,
-             "email": "test@mail.ru"
-         }
-         context = {
-             "title": request.POST.get("title"),
-             "author": request.POST.get("author"),
-             "content": request.POST.get("content"),
-             # "user": user
-         }
-         return render(request, 'article_view.html', context)
+        return render(request, 'create.html')
+    elif request.method == 'POST':
+        number_1 = request.POST.get('first_number')
+        number_2 = request.POST.get('second_number')
+        if request.POST.get('add') == 'add':
+            result = int(number_1) + int(number_2)
+            context = {
+                "first_number": number_1,
+                "second_number": number_2,
+                "sign": "+",
+                "equals": "=",
+                "result": result
+            }
+            return render(request, 'create.html', context)
+        elif request.POST.get('subtract') == 'subtract':
+            result = int(number_1) - int(number_2)
+            context = {
+                "first_number": number_1,
+                "second_number": number_2,
+                "sign": "-",
+                "equals": "=",
+                "result": result
+            }
+            return render(request, 'create.html', context)
+        elif request.POST.get('multiply') == 'multiply':
+            result = int(number_1) * int(number_2)
+            context = {
+                "first_number": number_1,
+                "second_number": number_2,
+                "sign": "*",
+                "equals": "=",
+                "result": result
+            }
+            return render(request, 'create.html', context)
+        elif request.POST.get('divide') == 'divide':
+            result = int(number_1) // int(number_2)
+            context = {
+                "first_number": number_1,
+                "second_number": number_2,
+                "sign": "//",
+                "equals": "=",
+                "result": result
+            }
+            return render(request, 'create.html', context)
